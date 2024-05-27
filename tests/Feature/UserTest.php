30a658cbcb4f782277558c3ce2a297e2d10abfd4
Pravin Chaudhary <pravinc.wot2022@gmail.com>
2024-05-27 13:13:00 +0530

    update yml file

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Models\User;
+
+test('user create', function () {
+    $user = User::factory()->create();
+
+    $response = $this->get('/');
+
+    $response->assertStatus(200);
+
+    expect(User::where('email', $user->email)->exists())->toBeTrue();
+});
